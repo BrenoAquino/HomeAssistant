b4c201ade58b17d7a9ac3dcd1640835a016cbc2f
BrenoAquino <brenoaquino7phr@gmail.com>
2023-07-20 10:11:24 -0300

    fix entity filter

---
+++ Features/Sources/Dashboard/UI/Views/EntityView/LightView.swift
@@ -0,0 +1,25 @@
+//
+//  LightView.swift
+//  
+//
+//  Created by Breno Aquino on 20/07/23.
+//
+
+import SwiftUI
+
+struct LightView: View {
+
+    var body: some View {
+        Text("")
+    }
+}
+
+#if DEBUG
+struct LightView_Preview: PreviewProvider {
+
+    static var previews: some View {
+
+        LightView()
+    }
+}
+#endif
