c1c122adb5f3b21552d75e7517e08d71a7aae873
BrenoAquino <brenoaquino7phr@gmail.com>
2023-07-23 13:32:45 -0300

    creating grid

--- Features/Sources/Dashboard/UI/DashboardView.swift
+++ Features/Sources/Dashboard/UI/DashboardView.swift
@@ -39,6 +39,9 @@ public struct DashboardView<ViewModel: DashboardViewModel>: View {
             Text("----")
             Text(viewModel.currentDashboard?.name ?? "nil")
             Text(String(describing: viewModel.currentDashboard?.entitiesIDs.count))
+            Text("----")
+
+            entitiesGrid
         }
         .navigationTitle(Localizable.hiThere.value)
         .toolbar {
@@ -55,6 +58,25 @@ public struct DashboardView<ViewModel: DashboardViewModel>: View {
             Localizable.done.text
         }
     }
+
+    private var entitiesGrid: some View {
+        GeometryReader { proxy in
+            let numberOfElementsInRow: Int = 3
+            let space = DSSpace.smallL.rawValue
+            let totalSpace = space * (CGFloat(numberOfElementsInRow) + 1)
+            let size = (proxy.size.width - totalSpace) / CGFloat(numberOfElementsInRow)
+            let columns = [GridItem](repeating: .init(.fixed(size), spacing: space), count: numberOfElementsInRow)
+
+            LazyVGrid(columns: columns) {
+                ForEach(viewModel.currentDashboard?.entitiesIDs ?? [], id: \.self) { entity in
+                    Text(entity)
+                        .padding()
+                        .frame(height: size)
+                        .background(Color.red)
+                }
+            }
+        }
+    }
 }
 
 #if DEBUG
@@ -63,16 +85,20 @@ import Preview
 struct DashboardView_Preview: PreviewProvider {
 
     class FakeViewModel: DashboardViewModel {
+
+        @Published var editModel: Bool = false
+        @Published var selectedDashboardIndex: Int? = 0
+        @Published var dashboards: [Dashboard] = DashboardMock.all
+        var currentDashboard: Domain.Dashboard? {
+            guard let selectedDashboardIndex else { return nil }
+            return dashboards[selectedDashboardIndex]
+        }
+
         var didSelectAddDashboard: (() -> Void)?
         var didSelectEditDashboard: ((Domain.Dashboard) -> Void)?
 
-        var editModel: Bool = false
-        var selectedDashboardIndex: Int?
-        var dashboards: [Dashboard] = []
-        var currentDashboard: Domain.Dashboard?
-
-        func didSelectAdd() {}
-        func didSelectEdit(_ dashboard: Dashboard) {}
+        func didSelectAdd() { print("didSelectAdd") }
+        func didSelectEdit(_ dashboard: Dashboard) { print("didSelectEdit \(dashboard.name)") }
     }
 
     static var previews: some View {
--- Features/Sources/Preview/DashboardService+Mock.swift
+++ Features/Sources/Preview/DashboardService+Mock.swift
@@ -10,15 +10,29 @@ import Combine
 import Domain
 import Foundation
 
+public enum DashboardMock {
+    public static let bedroom = Domain.Dashboard(name: "Bedroom", icon: "bed.double", entities: [
+        EntityMock.ledDeskLight.id, EntityMock.ledCeilingLight.id, EntityMock.fan.id
+    ])
+    public static let living = Domain.Dashboard(name: "Living Room", icon: "sofa", entities: [
+        EntityMock.climate.id
+    ])
+    public static let kitchen = Domain.Dashboard(name: "Kitchen", icon: "refrigerator", entities: [
+        EntityMock.coffeeMachine.id
+    ])
+    public static let garden = Domain.Dashboard(name: "Garden", icon: "tree", entities: [
+        EntityMock.mainLight.id, EntityMock.fan.id
+    ])
+    public static let security = Domain.Dashboard(name: "Security", icon: "light.beacon.max", entities: [
+        EntityMock.mainLight.id
+    ])
+
+    public static let all = [bedroom, living, kitchen, garden, security]
+}
+
 public class DashboardServiceMock: Domain.DashboardService {
 
-    public var dashboards: [Domain.Dashboard] = [
-        .init(name: "Bedroom", icon: "bed.double", entities: [""]),
-        .init(name: "Living Room", icon: "sofa", entities: [""]),
-        .init(name: "Kitchen", icon: "refrigerator", entities: [""]),
-        .init(name: "Garden", icon: "tree", entities: [""]),
-        .init(name: "Security", icon: "light.beacon.max", entities: [""]),
-    ]
+    public var dashboards: [Domain.Dashboard] = DashboardMock.all
 
     public init() {}
 
